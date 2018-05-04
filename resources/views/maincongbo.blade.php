18ca8d989ba9e0e92be35ae6e02a5b8c812f71aa
HuongVu <vuhuong297@mail.com>
2018-05-04 10:00:18 +0700

    update trien khai Vinh Phuc

--- resources/views/maincongbo.blade.php
+++ resources/views/maincongbo.blade.php
@@ -258,13 +258,13 @@ License: You must have a valid license purchased only from themeforest(the above
             <div class="col-md-3 col-sm-6 col-xs-12 footer-block">
                 <h2>About</h2>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam dolore.
+
                 </p>
             </div>
             <div class="col-md-3 col-sm-6 col-xs12 footer-block">
                 <h2>Subscribe Email</h2>
                 <div class="subscribe-form">
-                    <form action="javascript:;">
+                    <form action="">
                         <div class="input-group">
                             <input type="text" placeholder="mail@email.com" class="form-control">
 							<span class="input-group-btn">
@@ -278,33 +278,33 @@ License: You must have a valid license purchased only from themeforest(the above
                 <h2>Follow Us On</h2>
                 <ul class="social-icons">
                     <li>
-                        <a href="javascript:;" data-original-title="rss" class="rss"></a>
+                        <a href="" data-original-title="rss" class="rss"></a>
                     </li>
                     <li>
-                        <a href="javascript:;" data-original-title="facebook" class="facebook"></a>
+                        <a href="" data-original-title="facebook" class="facebook"></a>
                     </li>
                     <li>
-                        <a href="javascript:;" data-original-title="twitter" class="twitter"></a>
+                        <a href="" data-original-title="twitter" class="twitter"></a>
                     </li>
                     <li>
-                        <a href="javascript:;" data-original-title="googleplus" class="googleplus"></a>
+                        <a href="" data-original-title="googleplus" class="googleplus"></a>
                     </li>
                     <li>
-                        <a href="javascript:;" data-original-title="linkedin" class="linkedin"></a>
+                        <a href="" data-original-title="linkedin" class="linkedin"></a>
                     </li>
                     <li>
-                        <a href="javascript:;" data-original-title="youtube" class="youtube"></a>
+                        <a href="" data-original-title="youtube" class="youtube"></a>
                     </li>
                     <li>
-                        <a href="javascript:;" data-original-title="vimeo" class="vimeo"></a>
+                        <a href="" data-original-title="vimeo" class="vimeo"></a>
                     </li>
                 </ul>
             </div>
             <div class="col-md-3 col-sm-6 col-xs-12 footer-block">
                 <h2>Contacts</h2>
                 <address class="margin-bottom-40">
-                    Phone: 800 123 3456<br>
-                    Email: <a href="mailto:info@metronic.com">info@metronic.com</a>
+                    Phone: <br>
+                    Email:
                 </address>
             </div>
         </div>
